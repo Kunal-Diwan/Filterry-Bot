a85a7d2ed7554ffd0d13b96d7e62ce396f90c577
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-08-05 10:21:40 +0530

    Update misc.py

--- FilterryBot/modules/helper_funcs/misc.py
+++ FilterryBot/modules/helper_funcs/misc.py
@@ -54,7 +54,7 @@ def paginate_modules(chat_id, page_n: int, module_dict: Dict, prefix, chat=None)
              in module_dict.values()])
 
     pairs = [
-    modules[i * 3:(i + 1) * 2] for i in range((len(modules) + 2 - 1) // 2)
+    modules[i * 2:(i + 1) * 2] for i in range((len(modules) + 2 - 1) // 2)
     ]
 
     round_num = len(modules) / 2
@@ -63,16 +63,6 @@ def paginate_modules(chat_id, page_n: int, module_dict: Dict, prefix, chat=None)
         pairs.append((modules[-1], ))
     elif calc == 2:
         pairs.append((modules[-1], ))
-
-    max_num_pages = ceil(len(pairs) / 10)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 3:
-        pairs = pairs[modulo_page * 3:3 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🏡 Home 🏡", callback_data="bot_start"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton(text="Updates",url="https://t.me/DevelopedBots"),EqInlineKeyboardButton(text="Support",url="https://t.me/DevelopedBotz")]]
